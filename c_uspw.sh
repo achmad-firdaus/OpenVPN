a53cff3b45c601c5ca57bd7583ff86c47421e91d
achmad <77251566+achmad-firdaus@users.noreply.github.com>
2022-08-04 13:55:36 +0700

    Update c_uspw.sh

--- c_uspw.sh
+++ c_uspw.sh
@@ -3,9 +3,9 @@
 # Create By Achmad
 # Date: 04 August 2022
 # Note:
-# Usage for create user: bash ./c_uspw.sh YES {username} {password} StatikIP:{YES/NO} {IF Statik YES IPaddress}
-#                    EX: bash ./c_uspw.sh YES achmad password NO 0.0.0.0
-#                    EX: bash ./c_uspw.sh YES achmad password YES 172.16.1.200
+# Usage for create user: bash ./c_uspw.sh YES {username} {password} AutoLogin:{YES/NO} StatikIP:{YES/NO} {IF Statik YES IPaddress}
+#                    EX: bash ./c_uspw.sh YES achmad password YES NO 0.0.0.0
+#                    EX: bash ./c_uspw.sh YES achmad password YES YES 172.16.1.200
 # Usage for delete user: bash ./c_uspw.sh NO {username} 
 
 YES='YES'
@@ -14,38 +14,62 @@ create=$1
 
 if [ $create == $YES ]
 then
-  echo "Create New User";
-  
-  #variable create
-  username=$2
-  password=$3
-  stat_statik=$4
-  stat_statik_IP=$5
+    echo "Create New User";
 
-  # Add User:
-  sacli --user $username --key "type" --value "user_connect" UserPropPut
-  
-  # Password:
-  sacli --user $username --new_pass "$password" SetLocalPassword
-  
-  # CheckList Allow Auto-login:
-  sacli --user $username --key "prop_autologin" --value "true" UserPropPut
+    #variable create
+    username=$2
+    password=$3
+    autologin=$4
+    stat_statik=$5
+    stat_statik_IP=$6
 
-  if [ $stat_statik == $YES ]
-  then
-    echo "Use Statik IP";
-    sacli --user $username --key "conn_ip" --value "$stat_statik_IP" UserPropPut
-  else
-    echo "Use Dynamic IP";
-  fi
+    # Add User:
+    sacli --user $username --key "type" --value "user_connect" UserPropPut
+
+    # Password:
+    sacli --user $username --new_pass "$password" SetLocalPassword
+
+    if [ $autologin == $YES ]
+        then
+            echo "Check Auto Login";
+            # CheckList Allow Auto-login:
+            sacli --user $username --key "prop_autologin" --value "true" UserPropPut
+    else
+        echo "NO Check Auto Login";
+    fi
+
+    if [ $stat_statik == $YES ]
+        then
+            echo "Use Statik IP";
+            sacli --user $username --key "conn_ip" --value "$stat_statik_IP" UserPropPut
+    else
+        echo "Use Dynamic IP";
+    fi
+
+    if [ $stat_statik == $YES ]
+        then
+            echo "Use VPN Gateway";
+
+            #Count GATEWAY VPN
+            len=7
+            for i in {1..5}
+            do
+                len=$(($len+1))
+                # echo $len;
+                sacli --user testing --key "c2s_route.$i" --value "$len" UserPropPut
+                # echo "Welcome $i times";
+            done
+    else
+        echo "NO Use VPN Gateway";
+    fi
 
 else
-  echo "Delete User";
-  
-  #variable create
-  username=$2
-  
-  sacli --user $username UserPropDelAll
+    echo "Delete User";
+
+    #variable create
+    username=$2
+
+    sacli --user $username UserPropDelAll
   
 fi
 
