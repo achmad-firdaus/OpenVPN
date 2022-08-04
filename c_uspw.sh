fdbdea58421e0f11c7a36adf9be2b7014cfe9a63
achmad <77251566+achmad-firdaus@users.noreply.github.com>
2022-08-04 12:09:09 +0700

    Update c_uspw.sh

--- c_uspw.sh
+++ c_uspw.sh
@@ -1,24 +1,39 @@
 #!/bin/sh
 # Usage: bash ./c_uspw.sh "username" "password" "stat value YES/NO use statik IP"
 
-username=$1
-password=$2
-stat_statik=$3
-stat_statik_YES='YES'
+YES='YES'
 
-echo $username;
-echo $password;
+create=$i
 
-# Add User:
-sacli --user $username --key "type" --value "user_connect" UserPropPut
-# Password:
-sacli --user $username --new_pass "$password" SetLocalPassword
-# CheckList Allow Auto-login:
-sacli --user $username --key "prop_autologin" --value "true" UserPropPut
-
-if [ $stat_statik == $stat_statik_YES ]
+if [ $create == $YES ]
 then
-  echo "Use Statik IP";
+  echo "Create New User";
+  
+  #variable create
+  username=$2
+  password=$3
+  stat_statik=$4
+  stat_statik_subnets=$5
+
+  # Add User:
+  sacli --user $username --key "type" --value "user_connect" UserPropPut
+  
+  # Password:
+  sacli --user $username --new_pass "$password" SetLocalPassword
+  
+  # CheckList Allow Auto-login:
+  sacli --user $username --key "prop_autologin" --value "true" UserPropPut
+
+  if [ $stat_statik == $YES ]
+  then
+    echo "Use Statik IP";
+    sacli --user testing --key "conn_ip" --value "$stat_statik_subnets" UserPropPut
+  else
+    echo "Use Dynamic IP";
+  fi
+
 else
   echo "Use Dynamic IP";
 fi
+
+
