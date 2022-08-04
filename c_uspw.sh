e3bdb0a39d2e68858ff43a6706ba30b1a15d32a4
achmad <77251566+achmad-firdaus@users.noreply.github.com>
2022-08-04 11:59:38 +0700

    Update c_uspw.sh

--- c_uspw.sh
+++ c_uspw.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Usage: sh c_uspw.sh "username" "password" "stat value YES/NO use statik IP"
+# Usage: bash ./c_uspw.sh "username" "password" "stat value YES/NO use statik IP"
 
 username=$1
 password=$2
@@ -8,11 +8,17 @@ stat_statik_YES='YES'
 
 echo $username;
 echo $password;
-echo $stat_statik;
+
+# Add User:
+sacli --user $username --key "type" --value "user_connect" UserPropPut
+# Password:
+sacli --user $username --new_pass "$password" SetLocalPassword
+# CheckList Allow Auto-login:
+sacli --user $username --key "prop_autologin" --value "true" UserPropPut
 
 if [ $stat_statik == $stat_statik_YES ]
 then
-  echo "Statik IP";
+  echo "Use Statik IP";
 else
-  echo "Dynamic IP";
+  echo "Use Dynamic IP";
 fi
